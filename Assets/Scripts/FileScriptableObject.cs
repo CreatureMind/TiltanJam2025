957974bc7ea4cfc967db7725d9b9b992b768e8bc
CreatureMind <124280801+CreatureMind@users.noreply.github.com>
2025-08-14 22:55:29 +0300

    Added hand

---
+++ Assets/Scripts/FileScriptableObject.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FileScriptableObject", menuName = "Scriptable Objects/FileScriptableObject")]
+public class FileScriptableObject : ScriptableObject
+{
+    public string fileName;
+    public Sprite fileIcon;
+    public string ipAddress;
+}
