bb4dcc290f4320dfc938a79a88c8422048c43ae0
abb2k <amitayguy6@gmail.com>
2025-08-15 10:43:26 +0300

    trash bin added

---
+++ Assets/TrashBin.cs
@@ -0,0 +1,28 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class TrashBin : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out FileHandler file))
+        {
+            DeleteFile(file);
+        }
+    }
+
+    void DeleteFile(FileHandler file)
+    {
+        if (file.coughtHand != null)
+            file.coughtHand.ForceUngrip();
+
+        file.isEntered = false;
+        file.transform.DOKill();
+        file.transform.DOScale(Vector3.zero, .5f)
+            .SetEase(Ease.InBack, 1.2f);
+        file.transform.DOMove(transform.position, .5f)
+            .SetEase(Ease.InOutExpo, 2);
+
+        Destroy(file.gameObject, .5f);
+    }
+}
